b7bd7053453a78fcb41c7cd74fcd070cad69010f
msgloriayang <yanggloria91@gmail.com>
2023-03-26 15:56:41 -0500

    merge changes within the utils and src folder

---
+++ utils/generate-site.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const pageTemplate = require('./src/page-template')
+
+const writeFile = (data) => {
+  fs.writeFile('./dist/index.html', pageTemplate(data), err => {
+ if (err) {
+      reject(err);
+    return;
+    }
+
+    else {
+      console.log('File created! Inside the "dist" folder, your index.html file was created. Please open and enjoy your Team Profile website!')
+    }
+  })
+}
+
+module.exports = { writeFile };
