3a3b26753e440a2a8ee45ca8f2cc1c5c740b3729
msgloriayang <yanggloria91@gmail.com>
2023-03-26 19:13:38 -0500

    pushing last minute changes

--- _tests_/engineer.test.js
+++ _tests_/engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require("../lib/Engineer");
 
 test('tests if engineer is a new object created', () => {
-  const engineer = new Engineer('Justin', 44, 'justin@creed.com', 'justin69code')
+  const engineer = new Engineer('Justin', 44, 'justin@creed.com', '69')
 
   expect(engineer.name).toEqual(expect.any(String));
   expect(engineer.id).toEqual(expect.any(Number));
